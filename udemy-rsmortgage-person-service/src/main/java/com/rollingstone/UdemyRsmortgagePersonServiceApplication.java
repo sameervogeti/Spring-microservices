7b082fc6c7c5e0819aece2ddc1da54cc62153f44
sameer <sameer@localhost>
2017-07-04 11:04:59 +0530

    first

---
+++ udemy-rsmortgage-person-service/src/main/java/com/rollingstone/UdemyRsmortgagePersonServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rollingstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRsmortgagePersonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRsmortgagePersonServiceApplication.class, args);
+	}
+}
